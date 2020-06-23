3aa7d7613d63a230b6e93225402fc83615eadddb
Brian O'Sullivan <brian@tykus.ie>
2020-06-23 22:38:49 +0100

    Setting up resources and Laravel routing

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body class="antialiased bg-gray-800 text-gray-100">
+    <div id="app"></div>
+</body>
+</html>
